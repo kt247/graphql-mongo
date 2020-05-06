ff1f7efd1ae1aae95b784c4c749865e125137889
Khiem Tran <gia_khiem@live.com>
2020-05-06 15:55:36 +0700

    Lesson module

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+@ObjectType('Lesson')
+export class LessonType {
+  @Field(type => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
