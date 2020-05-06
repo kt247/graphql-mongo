10a870f6b4dad8d2ba643d9b0d003728ed77ac2d
Khiem Tran <gia_khiem@live.com>
2020-05-06 18:08:28 +0700

    Student module

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,27 @@
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
+import { StudentService } from './student.service';
+
+@Resolver(of => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query(returns => StudentType)
+  async student(@Args('id') id: string): Promise<Student> {
+    return this.studentService.getStudent(id);
+  }
+
+  @Query(returns => [StudentType])
+  async students(): Promise<Student[]> {
+    return this.studentService.getStudents();
+  }
+
+  @Mutation(returns => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    return this.studentService.createStudent(createStudentInput);
+  }
+}
